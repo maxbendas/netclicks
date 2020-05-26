c2753de9e0bd1003ce7a10c496e90f6b589f4a68
Max <maxbendas86@gmail.com>
2020-05-26 23:49:03 +0300

    day 1.5

--- script/script.js
+++ script/script.js
@@ -2,6 +2,8 @@
 
 const leftMenu = document.querySelector('.left-menu');
 const hamburger = document.querySelector('.hamburger');
+const tvShowsList = document.querySelector('.tv-shows__list');
+const modal = document.querySelector('.modal');
 
 // открытие, закрытие меню
 
@@ -25,5 +27,50 @@ leftMenu.addEventListener('click', (event) => {
     dropdown.classList.toggle('active')
     leftMenu.classList.add('openMenu')
     hamburger.classList.add('open')
+  }
+})
+
+// смена карточки 
+const changeImage = event => {
+  const card = event.target.closest('.tv-shows__item')
+  if (card) {
+    const img = card.querySelector('.tv-card__img')
+
+    if (img.dataset.backdrop) {
+      [img.dataset.backdrop, img.src] = [img.src, img.dataset.backdrop]
+    }
+  }
+
+}
+tvShowsList.addEventListener('mouseover', changeImage)
+tvShowsList.addEventListener('mouseout', changeImage)
+// document.querySelectorAll('.tv-card__img').forEach(item => {
+//   item.addEventListener('mouseenter', () => {
+// src = item.src
+// debugger
+//   item.src = item.dataset.backdrop
+// })
+// item.addEventListener('mouseleave', () => )
+// })
+
+// открытие модального окна
+
+tvShowsList.addEventListener('click', (event) => {
+  event.preventDefault();
+  const target = event.target;
+  const card = target.closest('.tv-card');
+
+  if (card) {
+    document.body.style.overflow = 'hidden'
+    modal.classList.remove('hide')
+  }
+})
+
+// закрытие модального окна
+
+modal.addEventListener('click', (event) => {
+  if (event.target.classList.contains('modal') || event.target.closest('.cross')) {
+    document.body.style.overflow = '';
+    modal.classList.add('hide')
   }
 })
